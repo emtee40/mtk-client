414e8a22aedfb870bc0fa5be04485e680976a027
cygnusx <cygnusx@gmail.com>
2021-11-30 13:24:56 +0100

    First part of GUI

--- gui.py
+++ gui.py
@@ -1,9 +1,12 @@
+import sys
 import time
+import mtk
+import mock
 
-from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QTextOption
-import sys
+from PyQt5.QtWidgets import *
+
 
 class asyncThread(QThread):
     sendToLog = pyqtSignal(str);
@@ -15,28 +18,33 @@ class asyncThread(QThread):
         self.function(self);
 
 def getDevInfo(self):
-    print("test");
-#Init the app window
-app = QApplication(sys.argv)
-w = QWidget()
-w.setFixedSize(500,600);
-w.setWindowTitle("MTKTools v2.0")
-
-logBox = QPlainTextEdit(w);
-logBox.setGeometry(11,150,478,410);
-logBox.setWordWrapMode(QTextOption.NoWrap);
-logBox.setReadOnly(True);
-logBox.show();
-w.show();
+    self.sendToLog.emit("test");
+    variables = mock.Mock()
+    variables.cmd = "stage"
+    mtk.Main(variables).run()
 
 def sendToLog(info):
-    t = time.localtime();
-    logBox.appendPlainTexT(time.strftime("[%H:%M:%S", t)+"]: "+info)
+        t = time.localtime();
+        logBox.appendPlainText(time.strftime("[%H:%M:%S", t)+"]: "+info)
 
-#Get the device info
-thread = asyncThread(app, 0, getDevInfo)
-thread.sendToLog.connect(sendToLog);
-thread.start();
+if __name__ == '__main__':
+    #Init the app window
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.setFixedSize(500,600);
+    w.setWindowTitle("MTKTools v2.0")
 
-#Run loop the app
-app.exec();
+    logBox = QPlainTextEdit(w);
+    logBox.setGeometry(11,150,478,410);
+    logBox.setWordWrapMode(QTextOption.NoWrap);
+    logBox.setReadOnly(True);
+    logBox.show();
+    w.show();
+
+    #Get the device info
+    thread = asyncThread(app, 0, getDevInfo)
+    thread.sendToLog.connect(sendToLog);
+    thread.start();
+
+    #Run loop the app
+    app.exec();
