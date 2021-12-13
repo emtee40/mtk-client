f9e42e70580df0fb2dd1c00a80fcdd66c141bb08
cygnusx <cygnusx@gmail.com>
2021-12-13 11:48:56 +0100

    Getting GPT works

--- gui_test.py
+++ gui_test.py
@@ -19,8 +19,10 @@ variables.debugmode = True
 
 loglevel = logging.INFO
 config = mtk.Mtk_Config(loglevel=loglevel)
+config.gpt_settings = mtk.gpt_settings(0,0,0)  #This actually sets the right GPT settings..
 MtkTool = mtk.Main(variables)
 mtkClass = mtk.Mtk(config=config, loglevel=loglevel)
+print(mtkClass.config.gpt_settings);
 if mtkClass.preloader.init():
     print("connected!")
 
@@ -30,6 +32,7 @@ res = da_handler.configure_da(mtkClass, preloader=None)
 # res = DA was loaded correctly
 if res:
     # Start commands here
+    print(mtkClass.mtk.config.gpt_settings);
     data, guid_gpt = mtkClass.daloader.get_gpt()
     if guid_gpt is None:
         print("Error reading gpt")
