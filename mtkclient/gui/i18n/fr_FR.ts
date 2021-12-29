14d2fd3f051314ec5260ed78322de3c58dc4de70
Bjoern Kerler <info@revskills.de>
2021-12-29 16:08:38 +0100

    Add french language support for gui

---
+++ mtkclient/gui/i18n/fr_FR.ts
@@ -0,0 +1,183 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_GB">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main_gui.ui" line="72"/>
+        <source>MTKClient v2.0</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="137"/>
+        <source>No phone detected.</source>
+        <translation>Pas de téléphone détecté</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="202"/>
+        <source>&lt;b&gt;Made by:&lt;/b&gt; Bjoern Kerler&lt;br/&gt;&lt;b&gt;Gui by:&lt;/b&gt; Geert-Jan Kreileman&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Credits:&lt;/b&gt;&lt;br/&gt;kamakiri [xyzz]&lt;br/&gt;linecode exploit [chimera]&lt;br/&gt;Chaosmaster&lt;br/&gt;and all contributers&lt;/p&gt;</source>
+        <translation>&lt;b&gt;Faite par:&lt;/b&gt; Bjoern Kerler&lt;br/&gt;&lt;b&gt;Gui par:&lt;/b&gt; Geert-Jan Kreileman&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Crédits:&lt;/b&gt;&lt;br/&gt;kamakiri [xyzz]&lt;br/&gt;linecode exploit [chimera]&lt;br/&gt;Chaosmaster&lt;br/&gt;et tous les contributeurs&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="223"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Please connect a Mediatek phone to continue.&lt;br/&gt;&lt;br/&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Hint:&lt;/span&gt; Power off the phone before connecting.&lt;br/&gt;&lt;span style=&quot; font-style:italic; color:#393939;&quot;&gt;For brom mode:&lt;/span&gt;&lt;span style=&quot; color:#393939;&quot;&gt;&lt;br/&gt;Press and hold vol up, vol dwn, or all hw buttons and connect usb.&lt;br/&gt;&lt;/span&gt;&lt;span style=&quot; font-style:italic; color:#393939;&quot;&gt;For preloader mode:&lt;/span&gt;&lt;span style=&quot; color:#393939;&quot;&gt;&lt;br/&gt;Don&apos;t press any hw button and connect usb.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Veuillez connecter un téléphone Mediatek pour continuer.&lt;br/&gt;&lt;br/&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Veillez à maintenir:&lt;/span&gt; Le téléphone éteint avant de le connecter.&lt;br/&gt;&lt;span style=&quot; font-style:italic; color:#393939;&quot;&gt;Pour le BROM Mode:&lt;/span&gt;&lt;span style=&quot; color:#393939;&quot;&gt;&lt;br/&gt;Pressez et maintenez Volume +, Volume - ou tous les boutons physiques et connectez à l&apos;USB.&lt;br/&gt;&lt;/span&gt;&lt;span style=&quot; font-style:italic; color:#393939;&quot;&gt;Pour le Preloader Mode:&lt;/span&gt;&lt;span style=&quot; color:#393939;&quot;&gt;&lt;br/&gt;Ne pressez aucun bouton physique et connectez à l&apos;USB.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="294"/>
+        <source>Show debug log</source>
+        <translation>Afficher le journal de débogage</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="363"/>
+        <source>&amp;Read Flash</source>
+        <translation>&amp;Lire Flash</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="370"/>
+        <source>&amp;Write Flash</source>
+        <translation>&amp;Écrire Flash</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="378"/>
+        <source>&amp;Erase Flash</source>
+        <translation>Effacer le &amp;flash </translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="385"/>
+        <source>&amp;Tools</source>
+        <translation>&amp;Outils</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="400"/>
+        <source>Read partition(s)</source>
+        <translation>Lire la ou les partitions</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="405"/>
+        <source>Read full flash</source>
+        <translation>Lire le flash complet</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="410"/>
+        <source>Read at offset</source>
+        <translation>Lire à un décalage</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="415"/>
+        <source>Write partition(s)</source>
+        <translation>Écrire la ou les partitions</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="420"/>
+        <source>Write full flash</source>
+        <translation>Écrire le flash complet</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="425"/>
+        <source>Write at offset</source>
+        <translation>Écrire à un décalage</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="430"/>
+        <source>Erase partitions(s)</source>
+        <translation>Effacer la ou les partitions</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="435"/>
+        <source>Erase at offset</source>
+        <translation>Effacer à un décalage</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="440"/>
+        <source>Read RPMB</source>
+        <translation>Lire RPMB</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="445"/>
+        <source>Write RPMB</source>
+        <translation>Écrire RPMB</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="450"/>
+        <source>Read preloader</source>
+        <translation>Lire preloader</translation>
+    </message>
+    <message>
+        <location filename="main_gui.ui" line="455"/>
+        <source>Generate RPMB keys</source>
+        <translation>Générer des clés RPMB</translation>
+    </message>
+</context>
+<context>
+    <name>partitionListWidget</name>
+    <message>
+        <location filename="readpart_gui.ui" line="14"/>
+        <source>Read partition(s)</source>
+        <translation>Lire la ou les partitions</translation>
+    </message>
+    <message>
+        <location filename="readpart_gui.ui" line="61"/>
+        <source>Dump GPT</source>
+        <translation>Lire GPT</translation>
+    </message>
+    <message>
+        <location filename="readpart_gui.ui" line="100"/>
+        <source>Ready to start...</source>
+        <translation>Prêt à commencer...</translation>
+    </message>
+    <message>
+        <location filename="readpart_gui.ui" line="156"/>
+        <source>Start</source>
+        <translation>Démarrer</translation>
+    </message>
+    <message>
+        <location filename="readpart_gui.ui" line="176"/>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+    <message>
+        <location filename="readpart_gui.ui" line="185"/>
+        <source>Select partitions to read</source>
+        <translation>Sélectionnez les partitions à lire</translation>
+    </message>
+    <message>
+        <location filename="readpart_gui.ui" line="195"/>
+        <source>Select all</source>
+        <translation>Tout sélectionner</translation>
+    </message>
+</context>
+<context>
+    <name>readWidget</name>
+    <message>
+        <location filename="readfull_gui.ui" line="14"/>
+        <source>Read full flash</source>
+        <translation>Lire le flash complet</translation>
+    </message>
+    <message>
+        <location filename="readfull_gui.ui" line="37"/>
+        <source>Start</source>
+        <translation>Démarrer</translation>
+    </message>
+    <message>
+        <location filename="readfull_gui.ui" line="57"/>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+    <message>
+        <location filename="readfull_gui.ui" line="71"/>
+        <source>Select options</source>
+        <translation>Sélectionnez les options</translation>
+    </message>
+    <message>
+        <location filename="readfull_gui.ui" line="78"/>
+        <source>Dump GPT</source>
+        <translation>Lire GPT</translation>
+    </message>
+    <message>
+        <location filename="readfull_gui.ui" line="98"/>
+        <source>Ready to start...</source>
+        <translation>Prêt à commencer...</translation>
+    </message>
+</context>
+</TS>
--- mtkclient/gui/i18n/generate_translation.sh
+++ mtkclient/gui/i18n/generate_translation.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 lrelease de_DE.ts
 lrelease en_GB.ts
+lrelease fr_FR.ts
