0ad4402a8edd0a8560375d15668070e526243bc3
Bjoern Kerler <info@revskills.de>
2021-12-29 18:19:37 +0100

    Fix window resize

--- mtkclient/gui/main_gui.py
+++ mtkclient/gui/main_gui.py
@@ -18,7 +18,7 @@ class Ui_MainWindow(object):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.setWindowModality(Qt.NonModal)
-        MainWindow.resize(746, 574)
+        MainWindow.resize(746, 602)
         sizePolicy = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -53,19 +53,43 @@ class Ui_MainWindow(object):
         self.actionGenerate_RPMB_keys.setObjectName(u"actionGenerate_RPMB_keys")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.spinner_pic = QLabel(self.centralwidget)
-        self.spinner_pic.setObjectName(u"spinner_pic")
-        self.spinner_pic.setGeometry(QRect(655, 42, 64, 64))
+        self.gridLayout_2 = QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.widget_3 = QWidget(self.centralwidget)
+        self.widget_3.setObjectName(u"widget_3")
         sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.widget_3.sizePolicy().hasHeightForWidth())
+        self.widget_3.setSizePolicy(sizePolicy1)
+        self.widget_3.setMinimumSize(QSize(70, 128))
+        self.pic = QLabel(self.widget_3)
+        self.pic.setObjectName(u"pic")
+        self.pic.setGeometry(QRect(0, 0, 71, 128))
+        sizePolicy2 = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.pic.sizePolicy().hasHeightForWidth())
+        self.pic.setSizePolicy(sizePolicy2)
+        self.pic.setMinimumSize(QSize(50, 128))
+        self.pic.setMaximumSize(QSize(87, 128))
+        self.pic.setPixmap(QPixmap(u"images/phone_notfound.png"))
+        self.pic.setScaledContents(True)
+        self.pic.setAlignment(Qt.AlignCenter)
+        self.pic.setWordWrap(False)
+        self.spinner_pic = QLabel(self.widget_3)
+        self.spinner_pic.setObjectName(u"spinner_pic")
+        self.spinner_pic.setGeometry(QRect(10, 40, 51, 51))
         sizePolicy1.setHeightForWidth(self.spinner_pic.sizePolicy().hasHeightForWidth())
         self.spinner_pic.setSizePolicy(sizePolicy1)
         self.spinner_pic.setPixmap(QPixmap(u"images/phone_loading.png"))
-        self.spinner_pic.setScaledContents(False)
+        self.spinner_pic.setScaledContents(True)
         self.spinner_pic.setAlignment(Qt.AlignCenter)
-        self.gridLayout = QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName(u"gridLayout")
+
+        self.gridLayout_2.addWidget(self.widget_3, 0, 2, 1, 1)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.title = QLabel(self.centralwidget)
         self.title.setObjectName(u"title")
         self.title.setEnabled(True)
@@ -84,43 +108,10 @@ class Ui_MainWindow(object):
         self.title.setWordWrap(False)
         self.title.setIndent(0)
 
-        self.gridLayout.addWidget(self.title, 0, 0, 1, 1)
+        self.verticalLayout.addWidget(self.title)
 
-        self.pic = QLabel(self.centralwidget)
-        self.pic.setObjectName(u"pic")
-        sizePolicy1.setHeightForWidth(self.pic.sizePolicy().hasHeightForWidth())
-        self.pic.setSizePolicy(sizePolicy1)
-        self.pic.setMinimumSize(QSize(60, 128))
-        self.pic.setMaximumSize(QSize(87, 128))
-        self.pic.setPixmap(QPixmap(u"images/phone_notfound.png"))
-        self.pic.setScaledContents(True)
-        self.pic.setAlignment(Qt.AlignCenter)
-        self.pic.setWordWrap(False)
-
-        self.gridLayout.addWidget(self.pic, 0, 3, 2, 1)
-
-        self.phoneInfoTextbox = QLabel(self.centralwidget)
-        self.phoneInfoTextbox.setObjectName(u"phoneInfoTextbox")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        sizePolicy2.setHorizontalStretch(0)
-        sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.phoneInfoTextbox.sizePolicy().hasHeightForWidth())
-        self.phoneInfoTextbox.setSizePolicy(sizePolicy2)
-        self.phoneInfoTextbox.setAlignment(Qt.AlignRight|Qt.AlignTop|Qt.AlignTrailing)
-        self.phoneInfoTextbox.setWordWrap(True)
-
-        self.gridLayout.addWidget(self.phoneInfoTextbox, 0, 2, 1, 1)
-
-        self.line = QFrame(self.centralwidget)
-        self.line.setObjectName(u"line")
-        self.line.setFrameShape(QFrame.HLine)
-        self.line.setFrameShadow(QFrame.Sunken)
-
-        self.gridLayout.addWidget(self.line, 2, 0, 1, 4)
-
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setSpacing(10)
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.logoPic = QLabel(self.centralwidget)
         self.logoPic.setObjectName(u"logoPic")
         sizePolicy1.setHeightForWidth(self.logoPic.sizePolicy().hasHeightForWidth())
@@ -131,20 +122,46 @@ class Ui_MainWindow(object):
         self.logoPic.setScaledContents(True)
         self.logoPic.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
 
-        self.horizontalLayout_2.addWidget(self.logoPic)
+        self.horizontalLayout.addWidget(self.logoPic)
 
         self.copyrightInfo = QLabel(self.centralwidget)
         self.copyrightInfo.setObjectName(u"copyrightInfo")
-        sizePolicy3 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        sizePolicy3 = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Minimum)
         sizePolicy3.setHorizontalStretch(0)
         sizePolicy3.setVerticalStretch(0)
         sizePolicy3.setHeightForWidth(self.copyrightInfo.sizePolicy().hasHeightForWidth())
         self.copyrightInfo.setSizePolicy(sizePolicy3)
 
-        self.horizontalLayout_2.addWidget(self.copyrightInfo)
+        self.horizontalLayout.addWidget(self.copyrightInfo)
+
+        self.phoneInfoTextbox = QLabel(self.centralwidget)
+        self.phoneInfoTextbox.setObjectName(u"phoneInfoTextbox")
+        sizePolicy3.setHeightForWidth(self.phoneInfoTextbox.sizePolicy().hasHeightForWidth())
+        self.phoneInfoTextbox.setSizePolicy(sizePolicy3)
+        self.phoneInfoTextbox.setAlignment(Qt.AlignRight|Qt.AlignTop|Qt.AlignTrailing)
+        self.phoneInfoTextbox.setWordWrap(True)
+
+        self.horizontalLayout.addWidget(self.phoneInfoTextbox)
 
 
-        self.gridLayout.addLayout(self.horizontalLayout_2, 3, 0, 1, 4)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+
+        self.gridLayout_2.addLayout(self.verticalLayout, 0, 0, 1, 1)
+
+        self.line_2 = QFrame(self.centralwidget)
+        self.line_2.setObjectName(u"line_2")
+        self.line_2.setFrameShape(QFrame.HLine)
+        self.line_2.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_2, 1, 0, 1, 4)
+
+        self.line = QFrame(self.centralwidget)
+        self.line.setObjectName(u"line")
+        self.line.setFrameShape(QFrame.HLine)
+        self.line.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line, 3, 0, 1, 4)
 
         self.status = QLabel(self.centralwidget)
         self.status.setObjectName(u"status")
@@ -156,24 +173,14 @@ class Ui_MainWindow(object):
         self.status.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
         self.status.setWordWrap(False)
 
-        self.gridLayout.addWidget(self.status, 1, 0, 1, 3)
+        self.gridLayout_2.addWidget(self.status, 2, 0, 1, 2)
 
-        self.logBox = QPlainTextEdit(self.centralwidget)
-        self.logBox.setObjectName(u"logBox")
-        self.logBox.setMinimumSize(QSize(722, 0))
-        self.logBox.setStyleSheet(u"")
-        self.logBox.setReadOnly(True)
-        self.logBox.setProperty("hidden", False)
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.widget_2 = QWidget(self.centralwidget)
+        self.widget_2.setObjectName(u"widget_2")
 
-        self.gridLayout.addWidget(self.logBox, 5, 0, 1, 4)
-
-        self.horizontalLayout_4 = QHBoxLayout()
-        self.horizontalLayout_4.setSpacing(0)
-        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.horizontalLayout_4.setSizeConstraint(QLayout.SetNoConstraint)
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        self.horizontalLayout_4.addItem(self.horizontalSpacer)
+        self.gridLayout.addWidget(self.widget_2, 2, 0, 1, 1)
 
         self.debugBtn = QPushButton(self.centralwidget)
         self.debugBtn.setObjectName(u"debugBtn")
@@ -183,14 +190,29 @@ class Ui_MainWindow(object):
         self.debugBtn.setAutoDefault(False)
         self.debugBtn.setFlat(False)
 
-        self.horizontalLayout_4.addWidget(self.debugBtn)
+        self.gridLayout.addWidget(self.debugBtn, 0, 1, 1, 1)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.gridLayout.addItem(self.horizontalSpacer, 0, 0, 1, 1)
+
+        self.logBox = QPlainTextEdit(self.centralwidget)
+        self.logBox.setObjectName(u"logBox")
+        sizePolicy4 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(0)
+        sizePolicy4.setHeightForWidth(self.logBox.sizePolicy().hasHeightForWidth())
+        self.logBox.setSizePolicy(sizePolicy4)
+        self.logBox.setMinimumSize(QSize(722, 0))
+        self.logBox.setStyleSheet(u"")
+        self.logBox.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.logBox.setReadOnly(True)
+        self.logBox.setProperty("hidden", False)
+
+        self.gridLayout.addWidget(self.logBox, 1, 0, 1, 2)
 
 
-        self.gridLayout.addLayout(self.horizontalLayout_4, 4, 0, 1, 4)
-
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        self.gridLayout.addItem(self.horizontalSpacer_2, 0, 1, 1, 1)
+        self.gridLayout_2.addLayout(self.gridLayout, 4, 0, 1, 4)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
@@ -245,12 +267,12 @@ class Ui_MainWindow(object):
         self.actionWrite_RPMB.setText(QCoreApplication.translate("MainWindow", u"Write RPMB", None))
         self.actionRead_preloader.setText(QCoreApplication.translate("MainWindow", u"Read preloader", None))
         self.actionGenerate_RPMB_keys.setText(QCoreApplication.translate("MainWindow", u"Generate RPMB keys", None))
+        self.pic.setText("")
         self.spinner_pic.setText("")
         self.title.setText(QCoreApplication.translate("MainWindow", u"MTKClient v2.0", None))
-        self.pic.setText("")
-        self.phoneInfoTextbox.setText(QCoreApplication.translate("MainWindow", u"No phone detected.", None))
         self.logoPic.setText("")
         self.copyrightInfo.setText(QCoreApplication.translate("MainWindow", u"<b>Made by:</b> Bjoern Kerler<br/><b>Gui by:</b> Geert-Jan Kreileman<br/><br/><b>Credits:</b><br/>kamakiri [xyzz]<br/>linecode exploit [chimera]<br/>Chaosmaster<br/>and all contributers</p>", None))
+        self.phoneInfoTextbox.setText(QCoreApplication.translate("MainWindow", u"No phone detected.", None))
         self.status.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Please connect a Mediatek phone to continue.<br/><br/><span style=\" font-weight:600;\">Hint:</span> Power off the phone before connecting.<br/><span style=\" font-style:italic; color:#393939;\">For brom mode:</span><span style=\" color:#393939;\"><br/>Press and hold vol up, vol dwn, or all hw buttons and connect usb.<br/></span><span style=\" font-style:italic; color:#393939;\">For preloader mode:</span><span style=\" color:#393939;\"><br/>Don't press any hw button and connect usb.</span></p></body></html>", None))
         self.debugBtn.setText(QCoreApplication.translate("MainWindow", u"Show debug log", None))
         self.readFlashMenu.setTitle(QCoreApplication.translate("MainWindow", u"&Read Flash", None))
