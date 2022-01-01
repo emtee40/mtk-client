3b4230f6e179ede75ccb66f858898686bc6cd232
R0rt1z2 <rogerortizleal@gmail.com>
2022-01-01 03:16:19 +0100

    gui: add catalan (es_CA) translation

---
+++ mtkclient/gui/i18n/es_CA.ts
@@ -0,0 +1,382 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_CA">
+<context>
+    <name>EraseFlashWindow</name>
+    <message>
+        <location filename="../eraseFlashPartitions.py" line="36"/>
+        <location filename="../eraseFlashPartitions.py" line="41"/>
+        <source> left</source>
+        <translation> restant</translation>
+    </message>
+    <message>
+        <location filename="../eraseFlashPartitions.py" line="127"/>
+        <source>Error reading gpt</source>
+        <translation>Error al llegir GPT</translation>
+    </message>
+    <message>
+        <location filename="../eraseFlashPartitions.py" line="129"/>
+        <source>Select partitions to erase</source>
+        <translation>Seleccionar particions per esborrar</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_gui.ui" line="32"/>
+        <location filename="../main_gui.ui" line="161"/>
+        <source>MTKClient v2.0</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="238"/>
+        <source>No phone detected.</source>
+        <translation>Cap dispositiu detectat.</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="225"/>
+        <source>&lt;b&gt;Made by:&lt;/b&gt; Bjoern Kerler&lt;br/&gt;&lt;b&gt;Gui by:&lt;/b&gt; Geert-Jan Kreileman&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Credits:&lt;/b&gt;&lt;br/&gt;kamakiri [xyzz]&lt;br/&gt;linecode exploit [chimera]&lt;br/&gt;Chaosmaster&lt;br/&gt;and all contributers&lt;/p&gt;</source>
+        <translation>&lt;b&gt;Creat per:&lt;/b&gt; Bjoern Kerler&lt;br/&gt;&lt;b&gt;Gui per:&lt;/b&gt; Geert-Jan Kreileman&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Crèdits:&lt;/b&gt;&lt;br/&gt;kamakiri [xyzz]&lt;br/&gt;linecode exploit [chimera]&lt;br/&gt;Chaosmaster&lt;br/&gt;i tots els contribuïdors&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="281"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Please connect a Mediatek phone to continue.&lt;br/&gt;&lt;br/&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Hint:&lt;/span&gt; Power off the phone before connecting.&lt;br/&gt;&lt;span style=&quot; font-style:italic; color:#393939;&quot;&gt;For brom mode:&lt;/span&gt;&lt;span style=&quot; color:#393939;&quot;&gt;&lt;br/&gt;Press and hold vol up, vol dwn, or all hw buttons and connect usb.&lt;br/&gt;&lt;/span&gt;&lt;span style=&quot; font-style:italic; color:#393939;&quot;&gt;For preloader mode:&lt;/span&gt;&lt;span style=&quot; color:#393939;&quot;&gt;&lt;br/&gt;Don&apos;t press any hw button and connect usb.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Si us plau connecteu un dispositiu Mediatek per continuar.&lt;br/&gt;&lt;br/&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Assegureu-vos de:&lt;/span&gt; Apagueu el dispositiu abans de connectar-lo.&lt;br/&gt;&lt;span style=&quot; font-style:italic; color:#393939;&quot;&gt;Per al mode BROM:&lt;/span&gt;&lt;span style=&quot; color:#393939;&quot;&gt;&lt;br/&gt;Premeu i manteniu premut el botó Volum +, Volum - o tots els botons i connecteu el cable USB.&lt;br/&gt;&lt;/span&gt;&lt;span style=&quot; font-style:italic; color:#393939;&quot;&gt;Per al mode Preloader:&lt;/span&gt;&lt;span style=&quot; color:#393939;&quot;&gt;&lt;br/&gt;No premeu cap botó i connecteu el cable USB.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="317"/>
+        <source>Show debug log</source>
+        <translation>Mostra registre de depuració</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="386"/>
+        <source>&amp;Read Flash</source>
+        <translation>&amp;Llegir Flash</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="395"/>
+        <source>&amp;Write Flash</source>
+        <translation>&amp;Escriure Flash</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="405"/>
+        <source>&amp;Erase Flash</source>
+        <translation>Esborrar Flash</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="412"/>
+        <source>&amp;Tools</source>
+        <translation>&amp;Eines</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="426"/>
+        <source>Read partition(s)</source>
+        <translation>Llegir partició(ns)</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="431"/>
+        <source>Read full flash</source>
+        <translation>Llergir el flash sencer</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="436"/>
+        <source>Read at offset</source>
+        <translation>Llegir en un offset</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="441"/>
+        <source>Write partition(s)</source>
+        <translation>Escriure la(es) partició(ns)</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="446"/>
+        <source>Write full flash</source>
+        <translation>Escriure el flash sencer</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="451"/>
+        <source>Write at offset</source>
+        <translation>Escriure en un offset</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="456"/>
+        <source>Erase partitions(s)</source>
+        <translation>Esborrar la(es) partició(ns)</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="461"/>
+        <source>Erase at offset</source>
+        <translation>Esborrar en un offset</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="466"/>
+        <source>Read RPMB</source>
+        <translation>Llegir RPMB</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="471"/>
+        <source>Write RPMB</source>
+        <translation>Escriure RPMB</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="476"/>
+        <source>Read preloader</source>
+        <translation>Llegir preloader</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="481"/>
+        <source>Generate RPMB keys</source>
+        <translation>Generar les claus RPMB</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="486"/>
+        <source>Read boot2</source>
+        <translation>Llegir boot2</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="491"/>
+        <source>Write preloader</source>
+        <translation>Escriure preloader</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="496"/>
+        <source>Write boot2</source>
+        <translation>Escriure boot2</translation>
+    </message>
+</context>
+<context>
+    <name>ReadFlashWindow</name>
+    <message>
+        <location filename="../readFlashPartitions.py" line="35"/>
+        <location filename="../readFlashPartitions.py" line="40"/>
+        <source> left</source>
+        <translation> restant</translation>
+    </message>
+    <message>
+        <location filename="../readFlashPartitions.py" line="68"/>
+        <source>Select output directory</source>
+        <translation>Seleccionar ubicació d&apos;emmagatzematge per als fitxers de sortida</translation>
+    </message>
+    <message>
+        <location filename="../readFlashPartitions.py" line="140"/>
+        <source>Error reading gpt</source>
+        <translation>Error al llegir GPT</translation>
+    </message>
+    <message>
+        <location filename="../readFlashPartitions.py" line="142"/>
+        <source>Select partitions to dump</source>
+        <translation>Seleccioneu les particions per llegir</translation>
+    </message>
+</context>
+<context>
+    <name>ReadFullFlashWindow</name>
+    <message>
+        <location filename="../readFull.py" line="25"/>
+        <source> left</source>
+        <translation> restant</translation>
+    </message>
+    <message>
+        <location filename="../readFull.py" line="163"/>
+        <source>Ready to dump </source>
+        <translation>A punt per llegir </translation>
+    </message>
+    <message>
+        <source>Ready to write </source>
+        <translation type="obsolete">A punt per escriure </translation>
+    </message>
+</context>
+<context>
+    <name>WriteFlashWindow</name>
+    <message>
+        <location filename="../writeFlashPartitions.py" line="34"/>
+        <location filename="../writeFlashPartitions.py" line="38"/>
+        <source> left</source>
+        <translation> restant</translation>
+    </message>
+    <message>
+        <location filename="../writeFlashPartitions.py" line="55"/>
+        <source>Select input directory</source>
+        <translation>Seleccionar el directori d'entrada</translation>
+    </message>
+    <message>
+        <location filename="../writeFlashPartitions.py" line="171"/>
+        <source>Set</source>
+        <translation>Escollir</translation>
+    </message>
+</context>
+<context>
+    <name>WriteFullFlashWindow</name>
+    <message>
+        <location filename="../writeFull.py" line="25"/>
+        <source> left</source>
+        <translation> restant</translation>
+    </message>
+    <message>
+        <location filename="../writeFull.py" line="147"/>
+        <source>Ready to write </source>
+        <translation>A punt per escriure </translation>
+    </message>
+</context>
+<context>
+    <name>generateKeysMenu</name>
+    <message>
+        <location filename="../toolsMenu.py" line="36"/>
+        <source>Select output directory</source>
+        <translation>Seleccionar ubicació d&apos;emmagatzematge per als fitxers de sortida</translation>
+    </message>
+</context>
+<context>
+    <name>partitionListWidget</name>
+    <message>
+        <location filename="../readpart_gui.ui" line="14"/>
+        <source>Read partition(s)</source>
+        <translation>Llegir la(es) partició(ns)</translation>
+    </message>
+    <message>
+        <location filename="../readpart_gui.ui" line="61"/>
+        <source>Dump GPT</source>
+        <translation>Llegir GPT</translation>
+    </message>
+    <message>
+        <location filename="../erasepart_gui.ui" line="125"/>
+        <location filename="../readpart_gui.ui" line="100"/>
+        <source>Ready to start...</source>
+        <translation>Preparat per començar...</translation>
+    </message>
+    <message>
+        <location filename="../erasepart_gui.ui" line="166"/>
+        <location filename="../readpart_gui.ui" line="156"/>
+        <source>Start</source>
+        <translation>Començar</translation>
+    </message>
+    <message>
+        <location filename="../erasepart_gui.ui" line="186"/>
+        <location filename="../readpart_gui.ui" line="176"/>
+        <source>Close</source>
+        <translation>Tancar</translation>
+    </message>
+    <message>
+        <location filename="../readpart_gui.ui" line="185"/>
+        <source>Select partitions to read</source>
+        <translation>Seleccioneu les particions per llegir</translation>
+    </message>
+    <message>
+        <location filename="../erasepart_gui.ui" line="45"/>
+        <location filename="../readpart_gui.ui" line="195"/>
+        <source>Select all</source>
+        <translation>Selecciona-ho tot</translation>
+    </message>
+    <message>
+        <location filename="../erasepart_gui.ui" line="14"/>
+        <source>Erase partition(s)</source>
+        <translation>Esborrar la(es) partició(ns)</translation>
+    </message>
+    <message>
+        <location filename="../erasepart_gui.ui" line="115"/>
+        <source>Select partitions to erase</source>
+        <translation>Seleccionar particions per esborrar</translation>
+    </message>
+</context>
+<context>
+    <name>readWidget</name>
+    <message>
+        <location filename="../readfull_gui.ui" line="14"/>
+        <source>Read full flash</source>
+        <translation>Llegir el flash sencer</translation>
+    </message>
+    <message>
+        <location filename="../readfull_gui.ui" line="37"/>
+        <source>Start</source>
+        <translation>Començar</translation>
+    </message>
+    <message>
+        <location filename="../readfull_gui.ui" line="57"/>
+        <source>Close</source>
+        <translation>Tancar</translation>
+    </message>
+    <message>
+        <location filename="../readfull_gui.ui" line="71"/>
+        <source>Select options</source>
+        <translation>Seleccioneu opcions</translation>
+    </message>
+    <message>
+        <location filename="../readfull_gui.ui" line="78"/>
+        <source>Dump GPT</source>
+        <translation>Llegir GPT</translation>
+    </message>
+    <message>
+        <location filename="../readfull_gui.ui" line="98"/>
+        <source>Ready to start...</source>
+        <translation>Preparat per començar...</translation>
+    </message>
+</context>
+<context>
+    <name>self.parent</name>
+    <message>
+        <location filename="../toolkit.py" line="124"/>
+        <source>Select output file</source>
+        <translation>Seleccioneu el nom del fitxer per desar</translation>
+    </message>
+    <message>
+        <location filename="../toolkit.py" line="137"/>
+        <source>Select input file</source>
+        <translation>Seleccionar fitxer d'entrada</translation>
+    </message>
+</context>
+<context>
+    <name>writeWidget</name>
+    <message>
+        <location filename="../writefull_gui.ui" line="14"/>
+        <source>Write full flash</source>
+        <translation>Escriure el flaix sencer</translation>
+    </message>
+    <message>
+        <location filename="../writefull_gui.ui" line="37"/>
+        <source>Start</source>
+        <translation>Començar</translation>
+    </message>
+    <message>
+        <location filename="../writefull_gui.ui" line="57"/>
+        <source>Close</source>
+        <translation>Tancar</translation>
+    </message>
+    <message>
+        <location filename="../writefull_gui.ui" line="84"/>
+        <source>Ready to start...</source>
+        <translation>Preparat per començar...</translation>
+    </message>
+</context>
+<context>
+    <name>writepartitionListWidget</name>
+    <message>
+        <location filename="../writepart_gui.ui" line="14"/>
+        <source>Write partition(s)</source>
+        <translation>Escriure la(es) partició(ns)</translation>
+    </message>
+    <message>
+        <location filename="../writepart_gui.ui" line="29"/>
+        <source>Select partitions to write</source>
+        <translation>Seleccionar particions per escriure</translation>
+    </message>
+    <message>
+        <location filename="../writepart_gui.ui" line="108"/>
+        <source>Ready to start...</source>
+        <translation>Preparat per començar...</translation>
+    </message>
+    <message>
+        <location filename="../writepart_gui.ui" line="117"/>
+        <source>Start</source>
+        <translation>Començar</translation>
+    </message>
+    <message>
+        <location filename="../writepart_gui.ui" line="137"/>
+        <source>Close</source>
+        <translation>Tancar</translation>
+    </message>
+    <message>
+        <location filename="../writepart_gui.ui" line="188"/>
+        <source>Select from directory</source>
+        <translation>Seleccionar fitxers d'un directori</translation>
+    </message>
+</context>
+</TS>
--- mtkclient/gui/i18n/generate_translation.sh
+++ mtkclient/gui/i18n/generate_translation.sh
@@ -3,3 +3,4 @@ lrelease de_DE.ts
 lrelease en_GB.ts
 lrelease fr_FR.ts
 lrelease es_ES.ts
+lrelease es_CA.ts
