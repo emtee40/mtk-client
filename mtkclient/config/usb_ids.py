272af9de38b1abb42fc86515af65d6346c7ced42
Bjoern Kerler <info@revskills.de>
2022-01-18 19:21:23 +0100

    Add Sony brom id

--- mtkclient/config/usb_ids.py
+++ mtkclient/config/usb_ids.py
@@ -7,4 +7,5 @@ default_ids = [
     [0x1004, 0x6000, 2], # LG Preloader
     [0x22d9, 0x0006, -1], # OPPO Preloader
     [0x0FCE, 0xF200, -1], # Sony Brom
+    [0x0FCE, 0xD1E9, -1], # Sony Brom XA1
 ]
