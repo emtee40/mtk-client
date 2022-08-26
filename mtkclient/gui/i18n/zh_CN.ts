8a8cdc699bf09a51e4c86ee5b7aadfd9658f8963
haotian2004 <3210467831@qq.com>
2022-08-26 19:07:23 +0800

    Update zh_CN(.ts/.qm)

---
+++ mtkclient/gui/i18n/zh_CN.ts
@@ -0,0 +1,505 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_gui.ui" line="32"/>
+        <location filename="../main_gui.ui" line="417"/>
+        <source>MTKClient v2.0</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="209"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Step 1:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Power off the phone&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>把手机关机</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="259"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Step 2:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Connect the USB cable, hold both volume buttons if needed&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>连接手机，按住音量键（可能需要）</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="481"/>
+        <source>&lt;b&gt;Made by:&lt;/b&gt; Bjoern Kerler&lt;br/&gt;&lt;b&gt;Gui by:&lt;/b&gt; Geert-Jan Kreileman&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Credits:&lt;/b&gt;&lt;br/&gt;kamakiri [xyzz]&lt;br/&gt;linecode exploit [chimera]&lt;br/&gt;Chaosmaster&lt;br/&gt;and all contributers&lt;/p&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="503"/>
+        <source>No phone detected.</source>
+        <translation>没有找到设备。</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="721"/>
+        <location filename="../main_gui.ui" line="794"/>
+        <source>Read partition(s)</source>
+        <translation>读分区</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="727"/>
+        <source>Dump GPT</source>
+        <translation>备份GPT（分区表）</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="801"/>
+        <location filename="../main_gui.ui" line="963"/>
+        <source>Select all partitions</source>
+        <translation>选中所有分区</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="746"/>
+        <source>Select partitions to read</source>
+        <translation>选择需要备份的分区</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="309"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;No connection? Try shorting the test point to ground&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>无法连接？尝试短接测试点到GND</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="364"/>
+        <source>Show Debug Log</source>
+        <translation>显示调试日志</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="559"/>
+        <source>Connection settings</source>
+        <translation>连接设置</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="809"/>
+        <location filename="../main_gui.ui" line="875"/>
+        <location filename="../main_gui.ui" line="1344"/>
+        <source>Write partition(s)</source>
+        <translation>写分区</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="827"/>
+        <source>Select partitions to write</source>
+        <translation>选择需要写的分区</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="882"/>
+        <source>Select from directory</source>
+        <translation>从文件夹选择</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="890"/>
+        <location filename="../main_gui.ui" line="956"/>
+        <source>Erase partition(s)</source>
+        <translation>格式化分区</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="946"/>
+        <source>Select partitions to erase</source>
+        <translation>选择要格式化的分区</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="971"/>
+        <source>Flash Tools</source>
+        <translation>烧写 工具</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="979"/>
+        <source>Read flash</source>
+        <translation>读取Flash</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="986"/>
+        <location filename="../main_gui.ui" line="1379"/>
+        <source>Read preloader</source>
+        <translation>读取Preloader</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="993"/>
+        <location filename="../main_gui.ui" line="1389"/>
+        <source>Read boot2</source>
+        <translation>读取boot2</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1000"/>
+        <location filename="../main_gui.ui" line="1369"/>
+        <source>Read RPMB</source>
+        <translation>读出RPMB</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1024"/>
+        <source>Write flash</source>
+        <translation>烧写Flash</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1031"/>
+        <location filename="../main_gui.ui" line="1394"/>
+        <source>Write preloader</source>
+        <translation>烧写Preloader</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1038"/>
+        <location filename="../main_gui.ui" line="1399"/>
+        <source>Write boot2</source>
+        <translation>烧写boot2</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1045"/>
+        <location filename="../main_gui.ui" line="1374"/>
+        <source>Write RPMB</source>
+        <translation>烧写RPMB</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1069"/>
+        <source>Erase preloader</source>
+        <translation>格式化Preloader</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1076"/>
+        <source>Erase boot2</source>
+        <translation>格式化boot2</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1083"/>
+        <source>Erase RPMB</source>
+        <translation>格式化RPMB</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1107"/>
+        <source>Lock bootloader</source>
+        <translation>锁定BootLoader</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1114"/>
+        <source>Unlock bootloader</source>
+        <translation>解锁Bootloader</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1124"/>
+        <source>Keys</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1210"/>
+        <source>Generate Keys</source>
+        <translation>生成 Keys</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1155"/>
+        <location filename="../main_gui.ui" line="1160"/>
+        <location filename="../main_gui.ui" line="1165"/>
+        <location filename="../main_gui.ui" line="1170"/>
+        <location filename="../main_gui.ui" line="1175"/>
+        <location filename="../main_gui.ui" line="1180"/>
+        <location filename="../main_gui.ui" line="1185"/>
+        <source>Neue Zeile</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1190"/>
+        <source>Type</source>
+        <translation>类型</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1195"/>
+        <source>Value</source>
+        <translation>值</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1203"/>
+        <source>Ready.</source>
+        <translation>准备。</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1218"/>
+        <source>Debug Log</source>
+        <translation>调试日志</translation>
+    </message>
+    <message>
+        <source>Instructions</source>
+        <translation type="vanished">Instructions</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1321"/>
+        <source>&amp;File</source>
+        <translation>&amp;文件</translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1329"/>
+        <source>Quit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1334"/>
+        <source>Read full flash</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1339"/>
+        <source>Read at offset</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1349"/>
+        <source>Write full flash</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1354"/>
+        <source>Write at offset</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1359"/>
+        <source>Erase partitions(s)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1364"/>
+        <source>Erase at offset</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1384"/>
+        <source>Generate RPMB keys</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1404"/>
+        <source>Unlock / Lock</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1409"/>
+        <source>Lock device</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_gui.ui" line="1414"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;退出</translation>
+    </message>
+</context>
+<context>
+    <name>ReadFlashWindow</name>
+    <message>
+        <location filename="../readFlashPartitions.py" line="41"/>
+        <source>Select output directory</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../readFlashPartitions.py" line="118"/>
+        <source>Ready to dump </source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>UnlockMenu</name>
+    <message>
+        <location filename="../toolsMenu.py" line="32"/>
+        <source>Generating...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../toolsMenu.py" line="43"/>
+        <source>Bootloader: </source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>UnlockWindow</name>
+    <message>
+        <location filename="../unlock_gui.ui" line="14"/>
+        <source>Lock / Unlock</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../unlock_gui.ui" line="26"/>
+        <source>Unlock</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../unlock_gui.ui" line="39"/>
+        <source>Lock</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../unlock_gui.ui" line="52"/>
+        <source>Close</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>WriteFlashWindow</name>
+    <message>
+        <location filename="../writeFlashPartitions.py" line="28"/>
+        <source>Select input directory</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>generateKeysMenu</name>
+    <message>
+        <location filename="../toolsMenu.py" line="77"/>
+        <source>Keys generated!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../toolsMenu.py" line="81"/>
+        <source>Generating...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../toolsMenu.py" line="82"/>
+        <source>Select output directory</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../toolsMenu.py" line="99"/>
+        <source>Generating keys</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../../mtk_gui" line="172"/>
+        <location filename="../../../mtk_gui" line="182"/>
+        <location filename="../../../mtk_gui" line="191"/>
+        <source> left</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../../mtk_gui" line="339"/>
+        <source>Error reading gpt</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../../mtk_gui" line="341"/>
+        <source>Select partitions to dump</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../../mtk_gui" line="380"/>
+        <source>Set</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../../mtk_gui" line="489"/>
+        <source>Phone detected:
+Reading model info...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../../mtk_gui" line="516"/>
+        <source>Error initialising. Did you install the drivers?</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>self.config</name>
+    <message>
+        <location filename="../../Library/mtk_dalegacy.py" line="1094"/>
+        <location filename="../../Library/mtk_dalegacy.py" line="1105"/>
+        <source>Error on sending brom stage</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_daxflash.py" line="1067"/>
+        <source>Reconnecting to preloader</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_daxflash.py" line="1074"/>
+        <source>Connected to preloader</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_main.py" line="166"/>
+        <source>Uploading stage 1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_main.py" line="169"/>
+        <source>Successfully uploaded stage 1, sending stage 2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_main.py" line="201"/>
+        <source>Done sending stage 2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_main.py" line="223"/>
+        <source>Stage2 verification passed.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_main.py" line="239"/>
+        <source>Done jumping stage2 at %08X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_preloader.py" line="138"/>
+        <location filename="../../Library/mtk_preloader.py" line="141"/>
+        <source>Status: Waiting for connection</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_preloader.py" line="149"/>
+        <source>Status: Handshake failed, retrying...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_preloader.py" line="158"/>
+        <source>Sync error. Please power off the device and retry.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_preloader.py" line="391"/>
+        <location filename="../../Library/mtk_preloader.py" line="398"/>
+        <location filename="../../Library/mtk_preloader.py" line="406"/>
+        <source>DA Error</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_preloader.py" line="614"/>
+        <source>Sending DA.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_preloader.py" line="618"/>
+        <source>Error on DA_Send cmd</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_preloader.py" line="622"/>
+        <source>Error on DA_Send address</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_preloader.py" line="626"/>
+        <source>Error on DA_Send size</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_preloader.py" line="630"/>
+        <source>Error on DA_Send sig_len</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Library/mtk_preloader.py" line="641"/>
+        <source>Error on DA_Send</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>self.parent</name>
+    <message>
+        <location filename="../toolkit.py" line="127"/>
+        <source>Select output file</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../toolkit.py" line="140"/>
+        <source>Select input file</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
