a90e928ce16eb46fecb967a49a74e1496e2d4299
Bjoern Kerler <info@revskills.de>
2023-01-17 16:23:16 +0100

    Update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name='mtkclient',
-    version='1.6.0',
+    version='1.6.1',
     packages=find_packages(),
     long_description=open("README.md").read(),
     scripts=['mtk','stage2'],
